9ef0ffd1445986a0aa5f4a81ca508ebbaf362541
Irina Antonishina <111454571+IrinaAntonishina@users.noreply.github.com>
2023-06-19 18:45:58 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
 
+const formEl = document.querySelector('.login-form')
+
+formEl.addEventListener('submit', onFormSubmit);
+
+const userData = {};
+
+function onFormSubmit(event){
+    event.preventDefault();
+    const {elements:{email,password}} = event.currentTarget;
+
+    if(email.value === '' || password.value === ''){
+        alert('Всі поля повинні бути заповненні!')
+    }
+   userData.email = email.value;
+   userData.password = password.value;
+
+   console.log(userData);
+   event.currentTarget.reset();
+}
+
