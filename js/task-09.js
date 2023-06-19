7c8498f3f67584d119b8905262fbd1aa82a54a20
Irina Antonishina <111454571+IrinaAntonishina@users.noreply.github.com>
2023-06-19 19:06:55 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const btnEl = document.querySelector('.change-color');
+const textColor = document.querySelector('.color');
+const body = document.querySelector('body');
+
+
+btnEl.addEventListener('click', () => {
+  textColor.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+})
